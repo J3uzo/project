c11b8ea73f3ec17b9448ea11d6678d823f97bf6b
J3uzo <149688307+J3uzo@users.noreply.github.com>
2025-12-05 11:13:18 +0300

    Update and rename roles.py to users.py

--- app/model/users.py
+++ app/model/users.py
@@ -5,11 +5,11 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database.database import Base
 
 if TYPE_CHECKING:
-    from app.model.roles import RoleModel
+    from app.model.users import UserModel
 
 
-class RoleModel(Base):
-    __tablename__ = "roles"
+class UserModel(Base):
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     admin: Mapped[str] = mapped_column(String(100), nullable=False)
     user: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
